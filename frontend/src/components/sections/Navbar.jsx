e6a3db9b696d39781d4b8df88610b008eda689f7
Jonathan Sherman <yonsher@gmail.com>
2025-04-08 16:17:10 -0400

    mb

--- frontend/src/components/sections/Navbar.jsx
+++ frontend/src/components/sections/Navbar.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  // Check for a token when the component mounts
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setLoggedIn(!!token);
+  }, []);
+
+  // Logout handler: remove token and redirect to login page
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setLoggedIn(false);
+    navigate('/PeerTutor/#/login');
+  };
+
   return (
     <div className='nav'>
-        <h1 className='nav-title'><a href='/PeerTutor/'>PeerTutor</a></h1>
-        <ul className='nav-list'>
-            <li className='nav-msg'><a href='/PeerTutor/#/messages'>Messages</a></li>
-            <li className='nav-faq'><a href='/PeerTutor/#/faq'>FAQ</a></li>
-            <li className='nav-login'><a href='/PeerTutor/#/login'>Login</a></li>
-        </ul>
+      <h1 className='nav-title'><a href='/PeerTutor/'>PeerTutor</a></h1>
+      <ul className='nav-list'>
+        <li className='nav-msg'><a href='/PeerTutor/#/messages'>Messages</a></li>
+        <li className='nav-faq'><a href='/PeerTutor/#/faq'>FAQ</a></li>
+        <li className='nav-login'>
+          {loggedIn ? (
+            <button onClick={handleLogout}>Logout</button>
+          ) : (
+            <a href='/PeerTutor/#/login'>Login</a>
+          )}
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
