1321b8b7d2bed7bb292ba05c8166c8bc2d092a44
Jonathan Sherman <yonsher@gmail.com>
2025-04-08 11:05:33 -0400

    g

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/PeerTutor' element={<Home />} />
+        <Route path='/PeerTutor/' element={<Home />} />
         <Route path='/PeerTutor/#/login' element={<Login />} />
         <Route path='/PeerTutor/#/signup'element={<Signup />}/>
         <Route path='/PeerTutor/#/faq'element={<FAQ />}/>
