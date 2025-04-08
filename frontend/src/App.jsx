c35f1394456137b0a453483f3d873df18c0ccf15
Matt-W1566 <matthew.weinstein08@gmail.com>
2025-04-08 09:17:27 -0400

    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/sections/Navbar'
 import Home from './components/pages/Home'
 import Login from './components/pages/Login'
+import Signup from './components/pages/Signup'
+import FAQ from './components/pages/FAQ'
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup'element={<Signup />}/>
+        <Route path='/faq'element={<FAQ />}/>
       </Routes>
     </Router>
   )
